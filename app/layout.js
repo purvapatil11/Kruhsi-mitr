4052597647192fa865deca85206d5e410dcd8386
saibhumi146 <saibhumishinde@gmail.com>
2026-03-17 23:57:10 +0530

    Setup Tailwind CSS, update layout and page, add new assets

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,15 @@
 import './globals.css'
+import { DM_Serif_Display, Rubik } from 'next/font/google'
+
+const rubik = Rubik({
+  subsets: ['latin'],
+  weight: ['400', '500', '700'],
+})
+
+const serif = DM_Serif_Display({
+  subsets: ['latin'],
+  weight: '400',
+})
 
 export const metadata = {
   title: 'Krushi Mitr',
@@ -8,7 +19,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={rubik.className}>
+        {children}
+      </body>
     </html>
   )
 }
